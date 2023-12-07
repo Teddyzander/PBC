e33f80a8f441f696566936e48a0ab795ba5a6d4f
Teddyzander <edwardalexandersmall@hotmail.co.uk>
2023-12-07 17:36:46 +1100

    Second argument is flag

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,15 @@ int main(int argc, char* argv[])
     for (int i = 0; i < argc; i++) {
         cout << argv[i] << std::endl;
     }
-    if (argv[1] == "server") {
+    if (argv[2] == "server") {
         batchpir_main_server(argc, argv);
     }
-    else if (argv[1] == "client") {
+    else if (argv[2] == "client") {
         batchpir_main_client(argc, argv);
     }
 
     else {
         std::cout << "No valid arguments given." << std::endl;
-        batchpir_main_server(argc, argv);
-        batchpir_main_client(argc, argv);
     }    
     return 0;
 }
